18cf19bad2ab55c1ffd7b92b027d77fedd5cafe6
Adrel410 <ogundipe_adeola@columbusstate.edu>
2022-10-15 19:26:07 +0000

    Updated

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var mongose = require('mongoose')
 var app = express();
 
 app.use('/static', express.static("public"));
-app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res){
-    res.send('Hello World');
-})
+const mongoDB = 'mongodb+srv://adrel410:Kenny@0609@cluster0.ndyixac.mongodb.net/?retryWrites=true&w=majority'
+mongose.connect(mongoDB)
+mongose.Promise = global.Promise;
+let db = mongose.connection;
+db.on('error', console.error.bind(console, "MongoDB connention error: "))
+
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
